0cde5767f90f45f93d502af6d63cbf9fe8f8a812
Paul Querna <pquerna@apache.org>
2010-10-09 01:34:33 -0700

    stub out init methods on native

---
+++ lib/backends/native/init.c
@@ -0,0 +1,58 @@
+/*
+ * Licensed to Selene developers ('Selene') under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * Selene licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "sln_brigades.h"
+#include "native.h"
+
+
+selene_error_t*
+sln_native_initilize()
+{
+  return SELENE_SUCCESS;
+}
+
+void
+sln_native_terminate()
+{
+}
+
+selene_error_t*
+sln_native_create(selene_t *s)
+{
+  sln_native_baton_t *baton;
+  SLN_ASSERT_CONTEXT(s);
+
+  baton = (sln_native_baton_t*) calloc(1, sizeof(*baton));
+  s->backend_baton = baton;
+
+  return SELENE_SUCCESS;
+}
+
+selene_error_t*
+sln_native_start(selene_t *s)
+{
+  SLN_ASSERT_CONTEXT(s);
+
+  return SELENE_SUCCESS;
+}
+
+selene_error_t*
+sln_native_destroy(selene_t *s)
+{
+
+  return SELENE_SUCCESS;
+}
---
+++ lib/backends/native/native.h
@@ -0,0 +1,31 @@
+/*
+ * Licensed to Selene developers ('Selene') under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * Selene licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _NATIVE_H_
+#define _NATIVE_H_
+
+#include "selene.h"
+#include "selene_error.h"
+#include "sln_buckets.h"
+#include "sln_types.h"
+#include "sln_assert.h"
+
+typedef struct {
+  int dummy;
+} sln_native_baton_t;
+
+#endif
